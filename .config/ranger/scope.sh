977ac67dc3fe184695dc7564547cffe1dd81ffe8
Nic <ricebalvids@gmail.com>
2024-12-29 19:45:29 +0100

    added pywal

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -55,11 +55,11 @@ handle_extension() {
             atool --list -- "${FILE_PATH}" && exit 5
             bsdtar --list --file "${FILE_PATH}" && exit 5
             exit 1;;
-        rar)
-            ## Avoid password prompt by providing empty password
-            unrar lt -p- -- "${FILE_PATH}" && exit 5
-            exit 1;;
-        7z)
+#        rar)
+#            ## Avoid password prompt by providing empty password
+#            unrar lt -p- -- "${FILE_PATH}" && exit 5
+#            exit 1;;
+        7z|rar)
             ## Avoid password prompt by providing empty password
             7z l -p -- "${FILE_PATH}" && exit 5
             exit 1;;
